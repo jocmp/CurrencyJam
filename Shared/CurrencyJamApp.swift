027956e4363264ff27403b17b963b5bd1ffbc94a
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2022-06-23 20:28:18 -0400

    Initial Commit

---
+++ Shared/CurrencyJamApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyJamApp.swift
+//  Shared
+//
+//  Created by jocmp on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyJamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
